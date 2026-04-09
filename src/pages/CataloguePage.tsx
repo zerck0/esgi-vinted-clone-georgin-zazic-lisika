2a1b960707a9ac0a4b261b9bbb37ba455f50cb10
Ivan <zazicivan@gmail.com>
2026-04-09 10:53:51 +0200

    feat: get all articles and add link to naviguate to their details pages

--- src/pages/CataloguePage.tsx
+++ src/pages/CataloguePage.tsx
@@ -1,3 +1,40 @@
+import { useQuery } from "@tanstack/react-query";
+import { api } from "../services/api";
+import type { Article } from "../types/article";
+import { Link } from "react-router-dom";
+
 export default function CataloguePage() {
-  return <h1>Catalogue</h1>;
+  const { data } = useQuery({
+    queryKey: ["articles"],
+    queryFn: () => api.get<Article[]>("api/articles")
+  });
+
+  if (!data || data.length === 0) {
+    return <p>
+      Aucun article trouvé.
+    </p>;
+  }
+
+  return (
+    <div>
+      {data.map((article) => (
+        <Link
+          key={article.id}
+          to={`/articles/${article.id}`}
+        >
+          <img
+            src={article.imageUrl}
+            alt={article.title}
+          />
+
+          <div className="p-3">
+            <h2>{article.title}</h2>
+            <p>
+              {article.price.toLocaleString("fr-FR", { style: "currency", currency: "EUR"})}
+            </p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
